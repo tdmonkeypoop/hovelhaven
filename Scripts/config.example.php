59acd486d53b7c3fdd02d1f2dfd87aeca3e40418
David Larsen <jdavidlarsen@gmail.com>
2017-08-01 17:39:56 +0000

    Again trying to fix .gitignore

--- Scripts/config.example.php
+++ Scripts/config.example.php
@@ -1,5 +1,5 @@
 <?php
-	define ("DB_USER", "tdmonkey");
+	define ("DB_USER", "tdmonkey"); //Change name to c9 username
 	define ("DB_PASSWORD", "");
 	define ("DB_DATABASE", "tavern");
 	define ("DB_HOST", "0.0.0.0");
